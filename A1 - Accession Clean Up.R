9d3d92f56e8e18780df91cf12fd7ae93fa896707
Andy Meyer <ameyer24@gmail.com>
2018-04-03 11:29:44 -0500

    Cleaned up the accession import process; divided by archives for import into ArchivesSpace.

--- A1 - Accession Clean Up.R
+++ A1 - Accession Clean Up.R
@@ -14,12 +14,8 @@ test <- read_csv(accession_file, col_names = acc_col_headings,skip = 1)
 
 # reformat date column
 test$accession_accession_date <- parse_date_time(test$Date_Acc, c("m/d/y"))
-# how many na in the date column?
-sum(is.na(test$Date_Acc))
-sum(is.na(test$accession_accession_date))
 
-
-# Divide Accession #
+# Divide Accession number
 # Split string on -
 test <- separate(test,acc_num,c("accession_number_1","accession_number_2","accession_number_3"), sep = "-")
 
@@ -29,7 +25,7 @@ test$accession_number_3 <- toupper(test$accession_number_3)
 
 
 
-test10 <- test %>%
+test <- test %>%
   mutate(prov1=ifelse(!is.na(name1),
                     ifelse(!is.na(organization),
                            paste(name1," (on behalf of",organization,")", sep = " "),
@@ -37,25 +33,11 @@ test10 <- test %>%
                     organization)) %>%
   mutate(accession_prov1 = paste(prov1, "- contact information may be in accession files"))
 
-test11 <- test10 %>%
+test <- test %>%
   mutate(accession_provenance = ifelse(!is.na(date_unknown),
                                        paste(accession_prov1,"(exact date of donation unknown)"),
                                        accession_prov1))
 
-
-## Drop these columns
-drop_col <- c("ID",
-              "Date_Acc",
-              "status",
-              "collection_num",
-              "size",
-              "notes",
-              "credit",
-              "date_pro",
-              "size_pp",
-              "proc_info",
-              "de_acc")
-test11 <- test10[,!names(test10) %in% drop_col]
               
 
 
@@ -65,12 +47,18 @@ cols_to_keep <- c("accession_accession_date",
                   "accession_number_1",
                   "accession_number_2",
                   "accession_number_3",
+                  "archives",
                   "accession_provenance")
-test_export <- subset(test11,select = cols_to_keep)
+test_export <- subset(test, select = cols_to_keep)
 
 ## Export as CSV for import
 # remove all na
 write.csv(test_export, file = "export.csv", na="", row.names = FALSE)
 
- ## Exporting All for OpenRefine
-write.csv(test10, file="export2.csv", na="")
+###############################################################################
+## Export by Repository
+###############################################################################
+SASS_export <- filter(test_export, archives == "SASS")
+SASS_export <- subset(SASS_export, select = -archives)
+write.csv(SASS_export, file = "sass_export.csv", na="", row.names = FALSE)
+
