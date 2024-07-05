091663e82ea9910bf10844951f0f10e2fd8081f0
Andy Meyer <ameyer24@gmail.com>
2024-07-05 15:13:34 -0500

    Update 0 - Setup.R
    minor edits

--- 0 - Setup.R
+++ 0 - Setup.R
@@ -5,6 +5,7 @@ install.packages("docxtractr")
 install.packages("tidyverse")
 install.packages("XML")
 install.packages("readtext")
+install.packages("blogdown")
 ## This is a test.
 ###############################################################################
 ## Load Libraries
@@ -14,4 +15,4 @@ library(tidyverse)
 library(XML)
 library(readtext)
 library(lubridate)
-
+##TEST.
